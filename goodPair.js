f5b4fc4c5acf2db44c0163589aaeb1c4fe904ff4
Yewe <82028523+ye-we@users.noreply.github.com>
2022-12-28 12:49:43 +0300

    Update goodPair.js

--- goodPair.js
+++ goodPair.js
@@ -1,5 +1,6 @@
 var numIdenticalPairs = function(nums) {
     let result = 0;
+    // compare each number with the rest of the numbers exluding the ones before the current number
     for(let i=0;i<nums.length;i++){
         for(let j=i+1; j<nums.length; j++){
             if(nums[i]==nums[j]){
