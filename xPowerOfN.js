9ebf794428362a682879bf580761425fcfed9f1a
Yewe <82028523+ye-we@users.noreply.github.com>
2023-02-12 14:51:02 +0300

    Create xPowerOfN.js

---
+++ xPowerOfN.js
@@ -0,0 +1,13 @@
+var myPow = function (x, n) {
+  if (n === 0) return 1;
+  if (n < 0) return 1 / myPow(x, -n);
+
+  let result = 1;
+  while (n > 0) {
+    if (n % 2 === 1) result *= x;
+    x *= x;
+    n = Math.floor(n / 2);
+  }
+
+  return result;
+}
